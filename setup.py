274c240f25a68df5871cb5183945c855873b8629
Fergal Walsh <fergal.walsh@metglobal.com>
2013-10-07 18:07:09 +0300

    Added more information to setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 setup(
     name='korpus',
-    py_modules=['korpus'],
     version='0.0.1',
     description='similarity made easy!',
+    long_description=open('README.rst').read(),
+    license='MIT',
     author='Metglobal',
     author_email='kadir.pekel@metglobal.com',
-    url='https://github.com/metglobal/korpus'
+    url='https://github.com/metglobal/korpus',
+	py_modules=['korpus'],
+    tests_require=[
+        'mock'
+    ]
 )
